549828f146497759f58928a0e02ae4fe79c4c90d
Minesh Patel <mpatel@teamibr.com>
2021-05-07 15:43:27 -0400

    added homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
+import HomePage from './homepage.component';
 
-class App extends Component {
-  render() {
-    return <div className='App'>Hello World</div>;
-  }
+function App(){
+  return <div>
+
+    <HomePage/>
+  </div>
 }
-
 export default App;
